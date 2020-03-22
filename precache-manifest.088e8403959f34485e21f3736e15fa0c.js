6b7f157e7d941a5758de6f1b57eae028cdedf51d
devparth <parthchaudhary69@gmail.com>
2020-03-22 23:25:15 +0530

    Updates

---
+++ precache-manifest.088e8403959f34485e21f3736e15fa0c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1a2644746e2f56b371f0007bfa23672",
+    "url": "/portfolio2020/index.html"
+  },
+  {
+    "revision": "78470bb6a0941ec13c0e",
+    "url": "/portfolio2020/static/css/main.7bb0129f.chunk.css"
+  },
+  {
+    "revision": "ece2cbf3c45dbe227e0d",
+    "url": "/portfolio2020/static/js/2.9d0768c8.chunk.js"
+  },
+  {
+    "revision": "6dfff6134e5b39145e69ee232cf5e1a1",
+    "url": "/portfolio2020/static/js/2.9d0768c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78470bb6a0941ec13c0e",
+    "url": "/portfolio2020/static/js/main.651575b7.chunk.js"
+  },
+  {
+    "revision": "f3c87a4f37e35d957d47",
+    "url": "/portfolio2020/static/js/runtime-main.b9bd6fb3.js"
+  },
+  {
+    "revision": "2ee22365bb7da72d8330590ca4cef301",
+    "url": "/portfolio2020/static/media/aayan-solution.2ee22365.jpg"
+  },
+  {
+    "revision": "213fa14ec8040f851e83fa784147cf2e",
+    "url": "/portfolio2020/static/media/icon-behance.213fa14e.svg"
+  },
+  {
+    "revision": "3e1c5f784db9f27f073049490610ccef",
+    "url": "/portfolio2020/static/media/icon-codepen.3e1c5f78.svg"
+  },
+  {
+    "revision": "a43769d10a398bf547a08ae2a924cb29",
+    "url": "/portfolio2020/static/media/icon-dribble.a43769d1.svg"
+  },
+  {
+    "revision": "dfec572f9ae62e05a842e7d79c52a56f",
+    "url": "/portfolio2020/static/media/icon-git.dfec572f.svg"
+  },
+  {
+    "revision": "3ac31475019a7adddc51defc80f3f8c7",
+    "url": "/portfolio2020/static/media/icon-linkedin.3ac31475.svg"
+  },
+  {
+    "revision": "767d2fefa77cb7ae46ade35b6853bebd",
+    "url": "/portfolio2020/static/media/logo.767d2fef.svg"
+  },
+  {
+    "revision": "566f55cd84221ca3f9aa4f68f134e0c6",
+    "url": "/portfolio2020/static/media/name-letters.566f55cd.svg"
+  },
+  {
+    "revision": "cba92ecb9f7219448bc8bfd5a89fa5a8",
+    "url": "/portfolio2020/static/media/profile-pic.cba92ecb.png"
+  },
+  {
+    "revision": "047ab14d1399428ac1c7001e4ddd916e",
+    "url": "/portfolio2020/static/media/skill-html.047ab14d.svg"
+  }
+]);
